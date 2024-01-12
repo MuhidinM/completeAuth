4b37d7b8413c0b182d95c217eb6ce5f4c96228ac
MuhidinM <misbahmuhidin3@gmail.com>
2024-01-12 09:58:40 +0300

    middleware and secure routes

---
+++ routes.ts
@@ -0,0 +1,16 @@
+/**
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+/**
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+/**
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+/**
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
